61a1b17197c3ee7616c689f7dff82d0e74c67666
Florian Roks <froks@noreply.users.github.com>
2021-02-16 16:22:46 +0000

    fix to work with newest ttrss

--- index.php
+++ index.php
@@ -33,7 +33,8 @@
     define('NO_SESSION_AUTOSTART', true);
 
     require_once "autoload.php";
-    require_once "db.php";
+    require_once "classes/db.php";
+    require_once "classes/api.php";
     require_once "db-prefs.php";
     require_once "functions.php";
     require_once "sessions.php";
